385f01cfc15a1dc4b52451de99a53d5925736d00
Tao <taole.chen@gmail.com>
2024-01-23 00:11:43 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Advent of Code!");
+        Console.WriteLine("... running script now ...");
+
+        AdventOfCode.Day01();
+    }
+}
